1c666cf85823819a993873cd9f5adb7433ea735a
Gano603 <ganogaming603@gmail.com>
2023-09-07 00:31:06 +0500

    Deploymaent Changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import { connectDB } from './database/Database.js';
 import { app } from './middlewares.js'
+import ServerlessHttp from 'serverless-http';
 
 connectDB();
-
 
 app.listen(process.env.PORT,()=>{
     console.log("Working")
