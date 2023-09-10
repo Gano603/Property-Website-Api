7620e4acac1624af3ba550bdaad47e1120371a09
Gano603 <ganogaming603@gmail.com>
2023-09-10 17:21:25 +0500

    changed samesite

--- utils/features.js
+++ utils/features.js
@@ -4,7 +4,7 @@ export const respone = (res, status, success, message, token, rest) => {
     res.status(status).cookie("Session", token === undefined ? null : token, {
         maxAge: token === undefined ? 0 : 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite:process.env.NODE_ENV==="development"? "none":"lax",
+        sameSite:process.env.NODE_ENV==="development"? "lax":"none",
         secure:process.env.NODE_ENV==="development"? false:true,
         credentials: true
     }).json({
