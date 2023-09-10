814f2b5e5fca36362dc3cc483ff96391c814aabb
Gano603 <ganogaming603@gmail.com>
2023-09-10 14:19:51 +0500

    changed login controller

--- utils/features.js
+++ utils/features.js
@@ -5,7 +5,8 @@ export const respone = (res, status, success, message, token, rest) => {
         maxAge: token === undefined ? 0 : 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite:process.env.NODE_ENV==="development"? "lax":"none",
-        secure:process.env.NODE_ENV==="development"? false:true
+        secure:process.env.NODE_ENV==="development"? false:true,
+        Credential:true
     }).json({
         success,
         message,
