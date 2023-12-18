34d0154cd8687e5376a1779f9c84aec5f0f05dc0
Gano603 <ganogaming603@example.com>
2023-12-18 13:00:21 +0500

    Cookie Fix

--- utils/features.js
+++ utils/features.js
@@ -3,10 +3,11 @@ export const respone = (res, status, success, message, token, rest) => {
     //15days session or the cookie will expire on undefined
     res.status(status).cookie("Session", token === undefined ? null : token, {
         maxAge: token === undefined ? 0 : 15 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
+        // httpOnly: true,
         sameSite:process.env.NODE_ENV==="development"? "none":"lax",
         secure:process.env.NODE_ENV==="development"? true:false,
-        credentials: true
+        credentials: true,
+        
     }).json({
         success,
         message,
